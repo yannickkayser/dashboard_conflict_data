ded94812cdcd4e40c08cae2028d000deebc3d4e3
jspldrch <jana.speldrich@students.uni-mannheim.de>
2025-11-18 19:15:56 +0100

    data fetched till 2020-10-21

--- src/fetch_GNEWS.py
+++ src/fetch_GNEWS.py
@@ -134,8 +134,8 @@ def fetch_articles_monthly(start_date, end_date):
 # 4. RUN SCRIPT
 # =============================
 if __name__ == "__main__":
-    start_date = datetime(2019, 4, 2)
-    end_date = datetime(2025, 11, 7)
+    start_date = datetime(2020, 10, 21)
+    end_date = datetime(2025, 11, 18)
 
     print(f"🚀 Fetching articles from {start_date.date()} to {end_date.date()} ...")
     fetch_articles_monthly(start_date, end_date)
